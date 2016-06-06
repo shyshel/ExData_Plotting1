39ca6205df88f167b826eab73108eb54ff70d22d
zoogenic <zoogenic@gmail.com>
2016-06-06 06:38:08 +0300

    task 2 completed

---
+++ plot2.R
@@ -0,0 +1,12 @@
+library(lubridate)
+library(data.table)
+setClass('myDate')
+setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
+cc <- c("character", "character", "numeric", rep("NULL",6))
+b <- fread("household_power_consumption.txt", na.strings="?", header=T, sep=";", colClasses = cc)
+b1 <- subset(b, Date == "1/2/2007" | Date == "2/2/2007")
+b1$DateTime <- as.POSIXct(strptime(paste(b1$Date, b1$Time, sep=" "), format = "%d/%m/%Y %H:%M:%S"))
+
+plot(b1$DateTime, b1$Global_active_power,type = 'l', xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot2.png", width = 480, height = 480)
+dev.off()
